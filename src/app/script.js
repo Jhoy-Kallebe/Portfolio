57737e52f179ac06034a46dc2e2b83a30c808898
Jhoy-Kallebe <kallebejhoy@gmail.com>
2023-01-21 21:31:06 -0300

    add container pop up

--- src/app/script.js
+++ src/app/script.js
@@ -1,4 +1,15 @@
-function toggle(){
+function changeTheme(){
     let html = document.documentElement;
     html.classList.toggle('light');
 }
+
+function openContainer(){
+    let container = document.querySelector('.open');
+    container.style.display = "initial";
+}
+
+function closeContainer(){
+    let container = document.querySelector('.open');
+    container.style.display = "none";
+}
+
