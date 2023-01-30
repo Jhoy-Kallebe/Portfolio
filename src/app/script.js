34dd38a2a79c317d1692bb2a61da6a4b8f339544
Jhoy-Kallebe <kallebejhoy@gmail.com>
2023-01-30 10:43:54 -0300

    update in buttons and animation in header/footer

--- src/app/script.js
+++ src/app/script.js
@@ -14,3 +14,16 @@ function closeContainer(){
     container.style.display = "none";
 }
 
+const toDownHeader = document.querySelector('.to-down-header');
+const toDownFooter = document.querySelector('.to-down-footer');
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 400) {
+      toDownHeader.classList.add("active");
+      toDownFooter.classList.add("active");
+    } else {
+      toDownHeader.classList.remove("active");
+      toDownFooter.classList.remove("active");
+    }
+})
+
